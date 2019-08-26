253911c22b51aab3d33bfee64bb9cd6d44ee826a
Nicolas SURBAYROLE <nsurbayrole@quarkslab.com>
2019-08-26 17:18:28 +0200

    Try correct appveyor

--- test/QBDITest.cpp
+++ test/QBDITest.cpp
@@ -16,13 +16,13 @@
  * limitations under the License.
  */
 
-#define CATCH_CONFIG_RUNNER
-#include <catch2/catch.hpp>
-
 #include <stdlib.h>
 #include <time.h>
 
 #include "Utility/LogSys.h"
+
+#define CATCH_CONFIG_RUNNER
+#include <catch2/catch.hpp>
 
 int main(int argc, char** argv) {
 
